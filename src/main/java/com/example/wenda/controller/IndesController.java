3ded02c066124dbbca67662d1705b4473d8e2aa8
Huabuxiu <1347638343@qq.com>
2019-03-23 15:13:31 +0800

    fist running

--- src/main/java/com/example/wenda/controller/IndesController.java
+++ src/main/java/com/example/wenda/controller/IndesController.java
@@ -1,7 +1,9 @@
 package com.example.wenda.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -14,9 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndesController {
 
-    @RequestMapping("/")
+    @RequestMapping(path = {"/","/index"})
     @ResponseBody
     public String index(){
         return "Hello world!";
+    }
+
+    @RequestMapping(path = {"/profile/{groupId}/{userId}"})
+    @ResponseBody
+    public String profile(@PathVariable("userId") int userId,
+                          @PathVariable("groupId") String groupId,
+                          @RequestParam("type") int type,
+                          @RequestParam("key") String key){
+        return String.format("Profile Page of %s/%d, t:%d/key:%s",groupId,userId,type,key);
     }
 }
